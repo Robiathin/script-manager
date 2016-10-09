965af15c21e33df1282830b830b7cd01ad9bfb9f
Robert Tate <rob@rtate.se>
2016-10-09 16:04:21 -0400

    Refactor test suite

--- test/check.pl
+++ test/check.pl
@@ -10,26 +10,62 @@ EOF
 my $script_contents = <<EOF;
 #!/usr/bin/env perl
 
-print "hello world\n";
-
+print "hello world\\n";
 EOF
 
-# Test list
-unless ($expected_output == `sm -l -p`) {
-	print STDERR "List check failed\n";
-	exit 1;
+my $other_script_contents = <<EOF;
+#!/usr/bin/env perl
+
+print "other script\\n";
+EOF
+
+my $result = 0;
+
+print "Testing execute...\t";
+
+if ("hello world\n" eq `sm -e test`) {
+	print "PASS\n"
+} else {
+	print "FAIL\n";
+	$result = 1;
 }
 
-# Test search
-unless ($expected_output == `sm -s -n te`) {
-	print STDERR "Search check failed\n";
-	exit 1;
+print "Testing list...\t";
+
+if ($expected_output eq `sm -l -p`) {
+	print "PASS\n";
+} else {
+	print "FAIL\n";
+	$result = 1;
 }
 
-# Test echo
-unless ($script_contents == `sm -E test -p`) {
-	print STDERR "Echo check failed\n";
-	exit 1;
+print "Testing search...\t";
+
+if ($expected_output eq `sm -s -n te`) {
+	print "PASS\n";
+} else {
+	print "FAIL\n";
+	$result = 1;
 }
 
-exit 0;
+print "Testing echo...\t";
+
+if ($script_contents eq `sm -E test -p`) {
+	print "PASS\n";
+} else {
+	print "FAIL\n";
+	$result = 1;
+}
+
+print "Testing replace...\t";
+
+`sm -r test -f test/test2.pl`;
+
+if ($other_script_contents eq `sm -E test -p`) {
+	print "PASS\n";
+} else {
+	print "FAIL\n";
+	$result = 1;
+}
+
+exit $result;

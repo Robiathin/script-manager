06e23213e69577fef69b61260650f7bf97aa7eb5
Robert Tate <rob@rtate.se>
2016-10-17 08:22:06 -0400

    Improve code style on file_util.c

--- file_util.c
+++ file_util.c
@@ -25,22 +25,30 @@ copy_file(char *from, char *to)
 {
 	FILE *from_file, *to_file;
 	from_file = fopen(from, "rb");
+
 	if (from_file == NULL)
 		return 1;
+
 	to_file = fopen(to, "wb");
+
 	if (to_file == NULL) {
 		fclose(from_file);
 		return 1;
 	}
+
 	int ch;
 	while (1) {
 		ch = fgetc(from_file);
+
 		if (feof(from_file))
 			break;
+
 		fputc(ch, to_file);
 	}
+
 	fclose(from_file);
 	fclose(to_file);
+
 	return 0;
 }
 
@@ -49,16 +57,22 @@ print_file(char *file_path)
 {
 	FILE *file;
 	file = fopen(file_path, "rb");
+
 	if (file == NULL)
 		return 1;
+
 	int ch;
 	while (1) {
 		ch = fgetc(file);
+
 		if (feof(file))
 			break;
+
 		printf("%c", ch);
 	}
+
 	fclose(file);
+
 	return 0;
 }
 
@@ -66,9 +80,12 @@ int
 make_executable(char *path)
 {
 	struct stat file_stat;
+
 	if (stat(path, &file_stat))
 		return 1;
+
 	if (chmod(path, file_stat.st_mode | S_IXOTH | S_IXGRP | S_IXUSR))
 		return 1;
+
 	return 0;
 }
